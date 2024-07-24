71ea1e28b2ce4f88e667fdaa1198516c648d1587
Tom Bensky <tbensky@gmail.com>
2024-07-24 15:10:06 -0700

    edits

--- cnnkernels.py
+++ cnnkernels.py
@@ -244,16 +244,19 @@ while True:
         plt.subplot(3,1,1)
         plt.plot(loss_track['epoch'],loss_track['loss'])
         plt.ylabel("Loss")
-        plt.title("Convolutional Network Training Loss")
+        plt.xticks([])
+        plt.title("Convolutional Network Training")
 
         plt.subplot(3,1,2)
         plt.plot(loss_track['epoch'],loss_track['train_correct'])
+        plt.xticks([])
         plt.ylabel("Train Correct")
 
         plt.subplot(3,1,3)
         plt.plot(loss_track['epoch'],loss_track['test_correct'])
         plt.ylabel("Test Correct")
         plt.xlabel("Epoch")
+        plt.xticks()
 
         plt.savefig("loss.png",dpi=300)
         plt.close()
